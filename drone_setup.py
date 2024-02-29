36cfb959bf19cccb5fdd9b7b4837a2b9b5694095
pedresp <pedro.espinosa@alumnos.upm.es>
2024-02-29 18:50:55 +0100

    added haskey and next_drone

--- drone_setup.py
+++ drone_setup.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import classes as c
+import utils
 
 drones_bag = {}
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html", drones_bag=drones_bag)
+    return render_template("index.html", drones_bag=drones_bag, next_drone=utils.next_drone(drones_bag))
 
 @app.route("/add_drone", methods=['POST'])
 def basic_add_drone():
@@ -16,8 +17,6 @@ def basic_add_drone():
     drone_id = my_data["drone_id"]
     drone_speed = float(my_data["drone_speed"])
     drone_acc = float(my_data["drone_acc"])
-
-    print(type(drone_id), type(drone_speed), type(drone_acc))
 
     drones_bag[drone_id] = c.Drone(drone_id, drone_speed, drone_acc)
     return redirect(url_for("index"))
