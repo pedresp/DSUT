5dd49f16b07f0cbe24884e1d3e12d27c53a48ef3
pedresp <pedro.espinosa@alumnos.upm.es>
2024-02-29 20:30:01 +0100

    added options modify/remove/duplicate drone

--- drone_setup.py
+++ drone_setup.py
@@ -21,5 +21,24 @@ def basic_add_drone():
     drones_bag[drone_id] = c.Drone(drone_id, drone_speed, drone_acc)
     return redirect(url_for("index"))
 
+@app.route("/action_on_drone", methods=['POST'])
+def modify_drone():
+    my_data = request.form.to_dict()
+
+    drone_id = my_data["drone_id"]
+    drone_speed = float(my_data["drone_speed"])
+    drone_acc = float(my_data["drone_acc"])
+
+    if (my_data['aod'] == 'modify_drone'):
+        drones_bag[drone_id].speed = drone_speed
+        drones_bag[drone_id].acc = drone_acc
+    elif (my_data['aod'] == 'remove_drone'):
+        del drones_bag[drone_id]
+    else:
+        new_drone = utils.next_drone(drones_bag)
+        drones_bag[new_drone] = c.Drone(new_drone, drone_speed, drone_acc)
+
+    return redirect(url_for("index"))
+
 if __name__ == '__main__':
     app.run(debug=True)
