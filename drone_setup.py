78a1f22fde3758a33043a4cbb9f56e8e2e20862e
pedresp <pedro.espinosa@alumnos.upm.es>
2024-03-01 11:28:06 +0100

    drone_templates for acc and speed

--- drone_setup.py
+++ drone_setup.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for
 import classes as c
 import utils
 
+import os
+import pwd
+
 drones_bag = {}
+ros_ws = 'drone_proy/ros_tfg'
+route = f"/home/{pwd.getpwuid(os.getuid()).pw_name}/{ros_ws}/src/simplesim"
+
+print(route)
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html", drones_bag=drones_bag, next_drone=utils.next_drone(drones_bag))
+    return render_template("webpage/index.html", drones_bag=drones_bag, next_drone=utils.next_drone(drones_bag))
 
 @app.route("/add_drone", methods=['POST'])
 def basic_add_drone():
@@ -37,6 +44,16 @@ def modify_drone():
     else:
         new_drone = utils.next_drone(drones_bag)
         drones_bag[new_drone] = c.Drone(new_drone, drone_speed, drone_acc)
+
+    return redirect(url_for("index"))
+
+@app.route("/generate_config", methods=['POST'])
+def generate_config():
+    for drone_key, drone_value in drones_bag.items():
+        content = render_template('files/drone_template.yaml', drone_id=drone_key, drone_speed=drone_value.speed, drone_acc=drone_value.acc)
+        print(f"{route}/config/{drone_key}.yaml")
+        with open(f"{route}/config/{drone_key}.yaml", 'w', encoding='utf-8') as outf:
+            outf.write(content)
 
     return redirect(url_for("index"))
 
