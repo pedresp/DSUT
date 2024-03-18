f4956a376f40a6ebc8b3d482d094780ee677b191
pedresp <pedro.espinosa@alumnos.upm.es>
2024-03-18 13:09:51 +0100

    added new characteristics for each drone

--- classes.py
+++ classes.py
@@ -1,6 +1,10 @@
 '''Class to represent a drone and its characteristics'''
 class Drone:
-    def __init__(self, id:str, speed:float =10, acc:float =10) -> None:
+    def __init__(self, id:str, speed:float =10, acc:float =10, tof:float =50.0, sweep_width:float =20.0, coordx:float =-200, coordy:float =0.0 ) -> None:
         self.id = id
         self.speed = speed
         self.acc = acc
+        self.tof = tof
+        self.sweep_width = sweep_width
+        self.coordx = coordx
+        self.coordy = coordy
--- drone_setup.py
+++ drone_setup.py
@@ -26,8 +26,12 @@ def basic_add_drone():
     drone_id = my_data["drone_id"]
     drone_speed = float(my_data["drone_speed"])
     drone_acc = float(my_data["drone_acc"])
+    drone_tof = float(my_data["drone_tof"])
+    drone_sweep_width = float(my_data["drone_sweep_width"])
+    drone_coordx = float(my_data["drone_coordx"])
+    drone_coordy = float(my_data["drone_coordy"])
 
-    drones_bag[drone_id] = c.Drone(drone_id, drone_speed, drone_acc)
+    drones_bag[drone_id] = c.Drone(drone_id, drone_speed, drone_acc, drone_tof, drone_sweep_width, drone_coordx, drone_coordy)
     return redirect(url_for("index"))
 
 '''Method to modify/delete or add a copy of an existing drone'''
@@ -38,15 +42,23 @@ def modify_drone():
     drone_id = my_data["drone_id"]
     drone_speed = float(my_data["drone_speed"])
     drone_acc = float(my_data["drone_acc"])
+    drone_tof = float(my_data["drone_tof"])
+    drone_sweep_width = float(my_data["drone_sweep_width"])
+    drone_coordx = float(my_data["drone_coordx"])
+    drone_coordy = float(my_data["drone_coordy"])
 
     if (my_data['aod'] == 'modify_drone'):
         drones_bag[drone_id].speed = drone_speed
         drones_bag[drone_id].acc = drone_acc
+        drones_bag[drone_id].tof = drone_tof
+        drones_bag[drone_id].sweep_width = drone_sweep_width
+        drones_bag[drone_id].coordx = drone_coordx
+        drones_bag[drone_id].coordy = drone_coordy
     elif (my_data['aod'] == 'remove_drone'):
         del drones_bag[drone_id]
     else:
         new_drone = utils.next_drone(drones_bag)
-        drones_bag[new_drone] = c.Drone(new_drone, drone_speed, drone_acc)
+        drones_bag[new_drone] = c.Drone(drone_id, drone_speed, drone_acc, drone_tof, drone_sweep_width, drone_coordx, drone_coordy)
 
     return redirect(url_for("index"))
 
@@ -55,7 +67,9 @@ described in the web page'''
 @app.route("/generate_config", methods=['POST'])
 def generate_config():
     for drone_key, drone_value in drones_bag.items():
-        content = render_template('files/drone_template.yaml', drone_id=drone_key, drone_speed=drone_value.speed, drone_acc=drone_value.acc)
+        content = render_template('files/drone_template.yaml', drone_id=drone_key, drone_speed=drone_value.speed, drone_acc=drone_value.acc, \
+                                  drone_tof=drone_value.tof, drone_sweep_width=drone_value.sweep_width, drone_coordx=drone_value.coordx, \
+                                  drone_coordy= drone_value.coordy)
         print(f"{route}/config/{drone_key}.yaml")
         with open(f"{route}/config/{drone_key}.yaml", 'w', encoding='utf-8') as outf:
             outf.write(content)
